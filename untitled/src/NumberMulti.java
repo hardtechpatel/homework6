79ef7d6b98c0bd7977bc3b3453d6433a4aa39707
hardtechpatel <hardtech.patel@gmail.com>
2019-08-03 23:12:53 +0100

    commit

---
+++ untitled/src/NumberMulti.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class NumberMulti {
+    public static void main(String args []){
+
+        Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter First IntNumber :");
+            int a = scanner.nextInt();
+            System.out.println("Enter Second IntNumber :");
+            int b = scanner.nextInt();
+
+            System.out.println(a*b);
+
+
+    }
+}
